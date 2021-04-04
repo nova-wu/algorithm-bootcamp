aa9b5424dc2c473055cdd3fd37093b612bdef382
nova <nova_woo@foxmail.com>
2021-04-05 01:47:59 +0800

    完善第四周作业

--- week_04/README.md
+++ week_04/README.md
@@ -4,4 +4,4 @@
 2. [位 1 的个数](https://leetcode-cn.com/problems/number-of-1-bits/): TwoSolution.java
 3. [2 的幂](https://leetcode-cn.com/problems/power-of-two/): ThreeSolution.java
 4. [颠倒二进制位](https://leetcode-cn.com/problems/reverse-bits/): FourSolution.java
-5. [实现 Trie (前缀树)](https://leetcode-cn.com/problems/implement-trie-prefix-tree/): FiveSolution.java
+5. [实现 Trie (前缀树)](https://leetcode-cn.com/problems/implement-trie-prefix-tree/): Trie.java
--- week_04/src/OneSolution.java
+++ week_04/src/OneSolution.java
@@ -1,19 +1,13 @@
-import java.util.Arrays;
-
 public class OneSolution {
 
-    public int minPathSum(int[][] grid) {
-        int [] dp = new int[grid[0].length];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        dp[0] = 0;
-
-        for(int i = 0; i < grid.length; i++){
-            dp[0]=dp[0]+grid[i][0];
-            for(int j = 1; j < grid[i].length; j++){
-                dp[j]=Math.min(dp[j-1],dp[j])+grid[i][j];
-            }
+    public int climbStairs(int n) {
+        int p, q = 0, r = 1;
+        for (int i = 1; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = p + q;
         }
-        return dp[grid[0].length-1];
+        return r;
     }
 
     public static void main(String[] args) {
